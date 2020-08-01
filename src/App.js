44be396865260f02949a213e80f92a9cc2363d61
Rodion888 <rodionrodion2010@gmail.com>
2020-08-02 00:34:22 +0300

    first and last commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from './components/card/index';
+import Container from './components/container';
+import { mockedCards } from './mock';
+
+import './main.css';
+
+let cardOne, cardTwo;
+
+const App = () => {
+  const [cards, setCards] = useState([])
+  const [pause, setPause] = useState(false)
+
+  const cardClick = card => {
+    const { id, flipped } = card
+
+    if (pause) return;
+    
+	  if (!flipped && !cardOne) {
+      cardOne = card
+		  setFlippedCard(id, true)
+	  } else {
+      cardTwo = card
+      setFlippedCard(id, true);
+      matchCheck();
+	  }
+  }
+
+  const matchCheck = () => {
+    if (cardOne.name === cardTwo.name) {
+      setTimeout(() => {
+        setCards(cards.map(card => {
+          if (cardOne.id === card.id) return {...cardOne, correct: true}
+          if (cardTwo.id === card.id) return {...cardTwo, correct: true}
+          return card
+        }))
+        resetTurns();
+      }, 800);
+    } else {
+      setPause(true);
+
+      setTimeout(() => {
+        setCards(cards.map(card => {
+          if (cardOne.id === card.id) return {...cardOne, flipped: false}
+          if (cardTwo.id === card.id) return {...cardTwo, flipped: false}
+          return card
+        }))
+
+        resetTurns();
+      }, 1800);
+    }
+  }
+  
+  const setFlippedCard = (id, flipped) => {
+    setCards(cards.map(card => {
+      if (card.id !== id) return card
+      return { ...card, flipped }
+    }))
+  }
+
+  const resetTurns = () => {
+    [cardOne, cardTwo] = [null, null];
+    setPause(false);
+  }
+
+  useEffect(() => {
+    shuffleCards()
+  }, []);
+
+  const shuffleCards = () => {
+    const cards = mockedCards.sort(() => Math.random() - 0.5);
+    setCards(cards)
+  };
+
+  return (
+    <Container>
+        {cards.map(card => <Card
+          key={card.id}
+          name={card.name}
+          style={card.style}
+          flipped={card.flipped}
+          correct={card.correct}
+          fasFab={card.fasFab}
+          cardClick={() => cardClick(card)}
+        />)}
+    </Container>
+  )
+};
+
+export default App;
---
+++ src/components/card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = ({ name, style, cardClick, flipped, correct, fasFab }) => {
+	return (
+		<div className={`card ${flipped ? 'flip' : ''} ${correct ? 'correct': ''}`}
+			 onClick={cardClick}
+			 data-card={name}>
+			<div className="back"></div>
+			<i className={`front ${fasFab} fa-${style || name} fa-5x`}></i>
+		</div>
+	)
+}
+
+export default Card;
